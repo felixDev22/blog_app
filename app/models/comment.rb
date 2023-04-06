80ce190d2368dd93f34a2eeb60576f0c8b9210a3
felixDev22 <felixnzomo@gmail.com>
2023-04-07 00:22:34 +0300

    Add comment model, add update comments counter

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ApplicationRecord
+    belongs_to :post
+    belongs_to :author, class_name: 'User'
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments_counter + 1)
+  end
+end
