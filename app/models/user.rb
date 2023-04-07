1f936c666bb69c5a31376e23ea58fe7ad8255063
felixDev22 <felixnzomo@gmail.com>
2023-04-07 09:20:59 +0300

    Add validation for postCounter

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :author_id
 
   validates :username, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
