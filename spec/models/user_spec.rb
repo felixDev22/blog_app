3ef589bc99ef4206fcd1a1f7e173410ea34bbadf
felixDev22 <felixnzomo@gmail.com>
2023-04-07 18:20:29 +0300

    Fine tune the user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,25 +3,28 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'validations' do
     it 'validates presence of name' do
-      user = User.new(name: nil, photo: 'http//:www.unsplash.com', bio: 'A doctor with 12years experience', posts_counter: 0)
+      user = User.new(name: nil, photo: 'http//:www.unsplash.com', bio: 'A doctor with 12years experience',
+                      posts_counter: 0)
       expect(user.valid?).to be_falsey
     end
 
     it 'validates name is not empty' do
-      user = User.new(name: '', photo: 'http//:www.unsplash.com', bio: 'A doctor with 12years experience', posts_counter: 0)
+      user = User.new(name: '', photo: 'http//:www.unsplash.com', bio: 'A doctor with 12years experience',
+                      posts_counter: 0)
       expect(user.valid?).to be_falsey
     end
   end
 
-RSpec::Matchers.define :be_ordered_by do |expected|
-  match do |actual|
-    actual.order(expected.to_s).to_a == actual.to_a
+  RSpec::Matchers.define :be_ordered_by do |expected|
+    match do |actual|
+      actual.order(expected.to_s).to_a == actual.to_a
+    end
   end
-end
 
   context '#recent_posts' do
-  it 'returns up to 3 posts, ordered by created_at in descending order' do
-      user = User.create!(name: 'bazu', photo: 'http//:www.unsplash.com', bio: 'A doctor with 12years experience', posts_counter: 0)
+    it 'returns up to 3 posts, ordered by created_at in descending order' do
+      user = User.create!(name: 'bazu', photo: 'http//:www.unsplash.com', bio: 'A doctor with 12years experience',
+                          posts_counter: 0)
       Post.create(author: user, title: 'Hello', text: 'This is my first post')
       Post.create(author: user, title: 'Hello', text: 'This is my second post')
       Post.create(author: user, title: 'Hello', text: 'This is my third post')
@@ -29,8 +32,8 @@ end
       Post.create(author: user, title: 'Hello', text: 'This is my fifth post')
       recent_posts = user.recent_posts
 
-    expect(recent_posts.size).to be <= 3
-    expect(recent_posts).to be_ordered_by('created_at DESC')
+      expect(recent_posts.size).to be <= 3
+      expect(recent_posts).to be_ordered_by('created_at DESC')
     end
   end
 end
