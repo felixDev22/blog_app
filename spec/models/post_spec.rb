9c934f1ac6b27f1cf398431e6538adf02c2311d5
felixDev22 <felixnzomo@gmail.com>
2023-04-07 22:10:59 +0300

    Try to fix linters 04

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   let(:user1) do
-    User.create(name: 'Bazu', photo: 'http//:www.unsplah.com', bio: 'A doctor with 12 years of experience', posts_counter: 0)
+    User.create(name: 'Bazu', photo: 'http//:www.unsplah.com', bio: 'A doctor with 12 years of experience',
+                posts_counter: 0)
   end
   let(:post) { Post.create(author: user1, title: 'Hello', text: 'This is my first post') }
 
