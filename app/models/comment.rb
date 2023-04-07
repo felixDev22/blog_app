439876fb7593339a6b9eb2cb6d5adcc4c5e52bf8
felixDev22 <felixnzomo@gmail.com>
2023-04-07 18:19:40 +0300

    Fine tune the comment test

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   def update_comments_counter
+    puts "Current post comments counter: #{post.comments_counter}"
     post.update(comments_counter: post.comments_counter + 1)
+    puts "New post comments counter: #{post.comments_counter}"
   end
 end
