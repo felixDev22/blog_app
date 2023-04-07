d89e37a4824adf5f92594d47c5b76c5e7cbaf46a
felixDev22 <felixnzomo@gmail.com>
2023-04-07 16:52:36 +0300

    Update post test fix errors

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,52 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  user1 = User.create(name: 'Bazu', photo: 'http//:www.unsplah.com', bio: 'A doctor with 12years experience')
-  post = Post.create(author: user1, title: 'Hello', text: 'Good morning team')
+  let(:user1) {
+    User.create(name: 'Bazu', photo: 'http//:www.unsplah.com', bio: 'A doctor with 12 years of experience',
+                posts_counter: 0)
+  }
+  let(:post) { Post.create(author: user1, title: 'Hello', text: 'This is my first post') }
 
-  it 'title should be present' do
-    check = user1.valid?
-
-    expect(check).to eq(true)
+  it 'validates presence of title' do
+    post.title = nil
+    expect(post).not_to be_valid
   end
 
-  it 'title should not be more than 250 ' do
-    post_title = post.title
-    post_length = post_title.size
-
-    expect(post_length).to be <= 250
+  it 'validates maximum length of title' do
+    post.title = 'a' * 251
+    expect(post).not_to be_valid
   end
 
-  it 'posts_counter should be greater than or equal to zero' do
-    comments_counter = post.comments_counter
+  it 'comments_counter should be greater than or equal to zero' do
+    comments_counter = post.comments.count
 
     expect(comments_counter).to be >= 0
   end
 
   it 'likes_counter should be greater than or equal to zero' do
-    likes_counter = post.likes_counter
+    likes_counter = post.likes.count
 
     expect(likes_counter).to be >= 0
   end
 
   it '#update_post_counter should increase post_counter by 1' do
-    post.update_post_counter
-    check = user1.posts_counter
-
-    expect(check).to be > 0
+    expect { post.update_post_counter }.to change { user1.posts_counter }.by(1)
   end
 
-  it '#recent_comments should show 5 recent comments on post' do
-    Comment.new(post: post, user_id: user1.id, text: 'Hi Team!').save
-    Comment.new(post: post, user_id: user1.id, text: 'Morning Ken!').save
-    Comment.new(post: post, user_id: user1.id, text: 'Good to see you!').save
-    Comment.new(post: post, user_id: user1.id, text: 'Great!').save
-    Comment.new(post: post, user_id: user1.id, text: 'Wow!').save
-    Comment.new(post: post, user_id: user1.id, text: 'What a great day!').save
-    Comment.new(post: post, user_id: user1.id, text: 'Nice to See you!').save
+  describe '#recent_comments' do
+    before do
+      10.times do |c|
+        Comment.create(text: "Comment #{c}", post_id: subject.id, author_id: 1)
+      end
+    end
+  end
 
-    recent_comments = post.recent_comments
-
-    expect(recent_comments.size).to eq(5)
+  it 'should return the 5 most recent comments' do
+    expect(subject.recent_comments).to eq(subject.comments.order(created_at: :desc).limit(5))
   end
 end
