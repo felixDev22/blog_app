599b70689e6305ab8332b091e91f7ea9eb2cc40a
felixDev22 <felixnzomo@gmail.com>
2023-04-07 19:52:41 +0300

    Update like test, fix errors, test is good

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   describe '#update_comments_counter' do
     let!(:user1) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 5) }
-    let!(:post) { Post.create(author: user1, title: 'Hello', text: 'This is my first post', likes_counter: 5, comments_counter: 5) }
+    let!(:post) {
+      Post.create(author: user1, title: 'Hello', text: 'This is my first post', likes_counter: 5, comments_counter: 5)
+    }
     let!(:comment) { Comment.create(user_id: user1.id, post_id: post.id, text: 'Hi Tom!') }
 
     it 'increments comments_counter by 1' do
