e45a3a1462ee3b07a228b1cbea089570df82936e
felixDev22 <felixnzomo@gmail.com>
2023-04-13 02:25:12 +0300

    Add link, update the user parameters in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,7 @@ class UsersController < ApplicationController
 
   def show
    @users = User.find(params[:id])
+   @user_posts = Post.where(author_id: @user.id)
+   @recent_posts = @user.recent_posts
   end
 end
