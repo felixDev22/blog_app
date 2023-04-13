57d2d39dc4cbe088b75c9e4e011c0e5abe511045
felixDev22 <felixnzomo@gmail.com>
2023-04-13 07:37:18 +0300

    Update users controller add required parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
   end
 
   def show
-   @users = User.find(params[:id])
-   @user_posts = Post.where(author_id: @user.id)
-   @recent_posts = @user.recent_posts
+    @user = User.find(params[:id])
+    @user_posts = Post.where(author_id: @user.id)
+    @recent_posts = @user.recent_posts
   end
 end
