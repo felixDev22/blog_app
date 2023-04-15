56dc38f652f28c70e2ce77df5f7f9e99a9c0341f
felixDev22 <felixnzomo@gmail.com>
2023-04-15 06:14:15 +0300

    Fine tune, fix errors,

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,22 @@
 class PostsController < ApplicationController
   def index
-  @id = params[:user_id]
-  @user = User.find(@id)
-  @user_posts = Post.where(author_id: @user.id)
-end
-
-
-  def show
-    @user = User.find(params[:user_id])
-    @post = Post.find(params[:id])
-    @comments = @post.comments
-    @like = Like.new
+    @id = params[:user_id]
+    @user = User.find(@id)
+    @user_posts = Post.where(author_id: @user.id)
   end
 
- def new
-  @post = Post.new
-  @user = current_user
-end
+  def show
+    @user = User.find_by(id: params[:user_id])
+    @post = Post.find_by(id: params[:id])
+    @post_comments = @post.comments.includes(:author)
+    @new_comment = Comment.new
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = @user.posts.new
+    render :new, locals: { post: @post }
+  end
 
   def create
     @user = current_user
