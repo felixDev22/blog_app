f483241f33e4bdc1ad056546d3238d8e49e6d7e4
felixDev22 <felixnzomo@gmail.com>
2023-04-17 16:16:08 +0300

    Update route, add root path for user to create acc

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
-        resources :likes, only: [:new, :create]
+  root "users#index"
+  resources :users, only: [:index, :show, :create]  do
+    resources :posts, only: [:index, :show, :new, :create] do 
+      resources :comments, only: [ :new, :create]
+      resources :likes, only: [:create]
     end
   end
 end
