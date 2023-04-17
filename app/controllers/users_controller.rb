3fca292d7fdafb809529309dbcf85bce2d56d92c
felixDev22 <felixnzomo@gmail.com>
2023-04-17 16:17:24 +0300

    Add includes on the user index and show methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.order(created_at: :asc)
+    @users = User.all.order(created_at: :asc).includes(:posts)
     @current_user = current_user
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(posts: %i[likes comments]).find(params[:id])
     @user_posts = Post.where(author_id: @user.id)
     @recent_posts = @user.recent_posts
   end
