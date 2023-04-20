2276b7f0476ecff7ec3f9a3bc5b29804e5f6b000
felixDev22 <felixnzomo@gmail.com>
2023-04-20 14:06:29 +0300

    Update test, All tests pass

--- spec/views/users_index_spec.rb
+++ spec/views/users_index_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
+require 'capybara/rspec'
 
 RSpec.feature 'UsersIndices', type: :feature do
-  let!(:user) { User.find(1) }
+  let!(:user) { User.create(name: 'Fatima', photo: 'https://www.img2link.com/images/2023/04/13/c2bbea766ec481f3d798809dd39eedb6.png', posts_counter: 0) }
 
   def image_exists?(url)
     response = Net::HTTP.get_response(URI.parse(url))
