f015412863404b15e717efa1dfaf00d8e9268059
felixDev22 <felixnzomo@gmail.com>
2023-04-22 18:37:25 +0300

    Update application, add configure permitted

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -23,4 +23,8 @@ class Api::ApplicationController < ActionController::API
       msg: 'Invalid Token'
     }
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation])
+  end
 end
